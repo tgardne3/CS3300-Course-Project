668c383e997bfd399f17fe7ca43460a532bbacab
ajaffer1 <140925215+ajaffer1@users.noreply.github.com>
2026-03-06 08:15:14 -0700

    Update app.js
    adding firebase configuration, analytics, and auth

--- web_page/app.js
+++ web_page/app.js
@@ -2,6 +2,39 @@
 names: Azam, Brandon, David, Trey
 class: CS3300
 */
+// for firebase and firebase auth
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPD10z05bZ2jJFEKtsfkjp9euqYV1PWdk",
+  authDomain: "fir-sandbox-2026.firebaseapp.com",
+  projectId: "fir-sandbox-2026",
+  storageBucket: "fir-sandbox-2026.firebasestorage.app",
+  messagingSenderId: "909983508788",
+  appId: "1:909983508788:web:5208aefd9edba56a895d61",
+  measurementId: "G-ZXW457681J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
+
+
 
 
 //collect and store data when submit button is clicked
